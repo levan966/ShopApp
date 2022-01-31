2d629f2c8077c02deea47e6933132e139c7e386a
Levan Pipia <levanpipia@Levans-MacBook-Pro-2.local>
2022-01-31 15:24:40 +0400

    error solved

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 import productsReducer from "./store/reducers/products";
